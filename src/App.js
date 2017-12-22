3bd58a6cc5c0b3591b0be1dbd50c04808c858787
Raymond Smith <raysmithdev@gmail.com>
2017-12-22 15:44:10 +0000

    ddd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux'
-import { fetchUsers } from './actions'
+// import { connect } from 'react-redux'
+// import { fetchUsers } from './actions'
 
 class App extends Component {
-
-  componentDidMount() {
-    this.props.dispatch(fetchUsers())
-    
-  }
+  //
+  // componentDidMount() {
+  //   this.props.dispatch(fetchUsers())
+  //
+  // }
 
   render() {
     return (
       <div className="App">
-        <p>{this.props.x}</p>
+        <p>hello</p>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    x: state.user.name
-  }
-
-}
-
-export default connect(mapStateToProps)(App);
+export default App
